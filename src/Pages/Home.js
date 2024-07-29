5f7a8e5e7dec3a8c38300780649a0c8b1221c6fa
Kevin <kevinwestnyc@gmail.com>
2024-07-29 12:08:31 -0400

    swap icon for video

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React, { Suspense, useRef } from 'react'
 import { Container, Grid, Hidden } from "@material-ui/core";
 import OMSLogo from "../Images/OMSLogo.webp"; // Ensure this path is correct
 import AppStoreLogo from "../Images/Download_On_AppStore.svg";
+import AppPreview from "../Images/AppPreview.mp4";
 
 
 export default function Home() {
@@ -9,7 +10,14 @@ export default function Home() {
   
   return (
     <div className="page-content">
-        <img src={OMSLogo} className="home-logo"/>
+        {/* <img src={OMSLogo} className="home-logo"/> */}
+        <video
+                src={AppPreview}
+                controls
+                autoPlay
+                loop
+                style={{ width: '95%', height: 'auto', maxWidth: '550px', border:'10px solid white', borderRadius:'10px' }} // Adjust maxWidth to fit your design needs
+            />
         {/* <div> */}
         <a href={appStoreLink} target="_blank" rel="noopener noreferrer" >
             <img src={AppStoreLogo} alt="Download on the App Store" className="app-store-button"/>
